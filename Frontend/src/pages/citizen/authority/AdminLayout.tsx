54e2feefcbdb1422aeea9ff9cc28968a19b0b522
Abhishek Prajapati <abhishhekprajapati07@gmail.com>
2026-09-01 13:44:53 +0530

    changed main

--- Frontend/src/pages/citizen/authority/AdminLayout.tsx
+++
@@ -1,3 +0,0 @@
-import { Outlet, NavLink } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { Home, AlertTriangle, FileText, MapPin, CheckSquare, BarChart3 } from "lucide-react
